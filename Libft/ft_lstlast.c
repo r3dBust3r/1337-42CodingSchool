caafb3f17c01a160ff2d9ea2237e9646659c0dfd
r3dBust3r <otmane.ki98@gmail.com>
2025-10-29 03:04:21 +0100

    ft_lstlast

---
+++ Libft/ft_lstlast.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ottalhao <ottalhao@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 02:50:41 by ottalhao          #+#    #+#             */
+/*   Updated: 2025/10/29 03:00:07 by ottalhao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*current;
+
+	current = lst;
+	while (current->next)
+		current = current->next;
+	return (current);
+}
