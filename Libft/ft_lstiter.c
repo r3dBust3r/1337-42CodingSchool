b19c1e1a54e9dd1500492ae2e9b906a5af6d124d
r3dBust3r <otmane.ki98@gmail.com>
2025-11-03 23:30:29 +0100

    ft_lstiter

---
+++ Libft/ft_lstiter.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ottalhao <ottalhao@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 23:24:45 by ottalhao          #+#    #+#             */
+/*   Updated: 2025/11/03 23:29:56 by ottalhao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*current;
+
+	if (!lst || !f)
+		return ;
+	current = lst;
+	while (current)
+	{
+		f(current->content);
+		current = current->next;
+	}
+}
