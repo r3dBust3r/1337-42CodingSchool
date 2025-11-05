66e3f8e7a1fb63b91ff9213c0f354d8e1620d4c8
r3dBust3r <otmane.ki98@gmail.com>
2025-11-05 19:01:13 +0100

    ft_itoa/ft_atoi

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,62 +6,51 @@
 /*   By: ottalhao <ottalhao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 17:44:10 by ottalhao          #+#    #+#             */
-/*   Updated: 2025/10/28 10:52:04 by ottalhao         ###   ########.fr       */
+/*   Updated: 2025/11/05 18:59:36 by ottalhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	get_len(char *s)
+static int	calc_len(int n)
 {
-	int	i;
+	int	len;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-static void	my_strrev(char *s)
-{
-	int		i;
-	char	temp;
-
-	i = 0;
-	while (i < get_len(s) / 2)
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n != 0)
 	{
-		temp = s[i];
-		s[i] = s[get_len(s) - i - 1];
-		s[get_len(s) - i - 1] = temp;
-		i++;
+		len++;
+		n /= 10;
 	}
+	return (len);
 }
 
 char	*ft_itoa(int n)
 {
+	int		len;
 	char	*asc_n;
-	int		negative;
-	int		i;
+	long	num;
 
-	asc_n = (char *)malloc(sizeof(char) * 26);
-	if (!asc_n)
-		return (NULL);
-	negative = 0;
-	if (n < 0)
+	len = calc_len(n);
+	asc_n = (char *)malloc(sizeof(char) * (len + 1));
+	num = n;
+	asc_n[len] = '\0';
+	if (num < 0)
 	{
-		n = -n;
-		negative = 1;
+		asc_n[0] = '-';
+		num = -num;
 	}
-	i = 0;
-	while (n > 9)
+	if (num == 0)
 	{
-		asc_n[i++] = (n % 10) + '0';
-		n /= 10;
+		asc_n[0] = '0';
+		return (asc_n);
 	}
-	asc_n[i] = n + '0';
-	if (negative)
-		asc_n[++i] = '-';
-	my_strrev(asc_n);
-	asc_n[++i] = 0x0;
+	while (num > 0)
+	{
+		asc_n[--len] = (num % 10) + '0';
+		num /= 10;
+	}
 	return (asc_n);
 }
