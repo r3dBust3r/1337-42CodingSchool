373e3093432f4b7102285fadede25b5298c26d8e
r3dBust3r <otmane.ki98@gmail.com>
2025-11-05 16:54:20 +0100

    ft_lstlast_bonus

--- Libft/ft_lstlast_bonus.c
+++ Libft/ft_lstlast_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ottalhao <ottalhao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 02:50:41 by ottalhao          #+#    #+#             */
-/*   Updated: 2025/10/29 03:00:07 by ottalhao         ###   ########.fr       */
+/*   Updated: 2025/11/05 16:53:56 by ottalhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	t_list	*current;
-
-	current = lst;
-	while (current->next)
-		current = current->next;
-	return (current);
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
