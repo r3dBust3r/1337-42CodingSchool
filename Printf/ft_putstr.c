2315b416ed7eed8d6b35ab078f52abbaad1791a5
r3dBust3r <otmane.ki98@gmail.com>
2025-11-21 00:26:47 +0100

    Fixing (Seg-fault) When char *s == NULL

--- Printf/ft_putstr.c
+++ Printf/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: ottalhao <ottalhao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 18:58:30 by ottalhao          #+#    #+#             */
-/*   Updated: 2025/11/20 21:02:13 by ottalhao         ###   ########.fr       */
+/*   Updated: 2025/11/21 00:14:48 by ottalhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 void	ft_putstr(char *s, unsigned int *counter)
 {
 	int i = 0;
+	if (!s)
+	{
+		ft_putstr("(null)", counter);
+		return;
+	}
+
 	while (s[i])
 	{
 		ft_putchar(s[i], counter);
