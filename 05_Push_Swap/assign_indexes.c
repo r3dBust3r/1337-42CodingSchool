c2c815c57c9de3a650dc99a03014ac80f5880cd3
r3dBust3r <otmane.ki98@gmail.com>
2026-01-04 12:31:46 +0100

    norm: assign_indexes.c

--- 05_Push_Swap/assign_indexes.c
+++ 05_Push_Swap/assign_indexes.c
@@ -6,44 +6,49 @@
 /*   By: ottalhao <ottalhao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 14:17:05 by ottalhao          #+#    #+#             */
-/*   Updated: 2026/01/04 11:35:01 by ottalhao         ###   ########.fr       */
+/*   Updated: 2026/01/04 12:28:59 by ottalhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void assign_indexes(t_list **lst)
+t_list	*get_first_unindexed(t_list *lst)
 {
-	unsigned int count = count_lst(lst);
-	unsigned int index = 0;
-	unsigned int i = 0;
-	while (i < count)
+	while (lst)
 	{
-		t_list *smallest_node = NULL;
-		t_list *current = *lst;
-		while (current)
-		{
-			if (current->index == -1)
-			{
-				smallest_node = current;
-				break;
-			}
-			current = current->next;
-		}
-		current = *lst;
-		while (current)
-		{
-			if (current->index == -1)
-			{
-				if (current->n < smallest_node->n)
-				{
-					smallest_node = current;
-				}
-			}
-			current = current->next;
-		}
-		smallest_node->index = index;
+		if (lst->index == -1)
+			return (lst);
+		lst = lst->next;
+	}
+	return (NULL);
+}
+
+t_list	*get_smallest_unindexed(t_list *lst)
+{
+	t_list	*smallest;
+
+	smallest = get_first_unindexed(lst);
+	while (lst)
+	{
+		if (lst->index == -1 && lst->n < smallest->n)
+			smallest = lst;
+		lst = lst->next;
+	}
+	return (smallest);
+}
+
+void	assign_indexes(t_list **lst)
+{
+	unsigned int	count;
+	unsigned int	index;
+	t_list			*smallest;
+
+	count = count_lst(lst);
+	index = 0;
+	while (index < count)
+	{
+		smallest = get_smallest_unindexed(*lst);
+		smallest->index = index;
 		index++;
-		i++;
 	}
 }
