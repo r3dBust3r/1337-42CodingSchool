7baefcc71a2ac557eb48543a904182b82070b834
r3dBust3r <otmane.ki98@gmail.com>
2026-01-04 12:32:58 +0100

    norm: find_distance.c

--- 05_Push_Swap/find_distance.c
+++ 05_Push_Swap/find_distance.c
@@ -6,7 +6,7 @@
 /*   By: ottalhao <ottalhao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 14:16:30 by ottalhao          #+#    #+#             */
-/*   Updated: 2026/01/04 11:34:24 by ottalhao         ###   ########.fr       */
+/*   Updated: 2026/01/04 12:32:34 by ottalhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 int	find_distance(t_list *lst, int n)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (lst)
 	{
 		if (lst->n == n)
-			break;
+			break ;
 		lst = lst->next;
 		i++;
 	}
-	return i;
+	return (i);
 }
