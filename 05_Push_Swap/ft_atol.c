db6300c7eadf99154d65c3bed3c821cc636994fe
r3dBust3r <otmane.ki98@gmail.com>
2026-01-04 16:28:31 +0100

    Handling: Overflow on Long Long

--- 05_Push_Swap/ft_atol.c
+++ 05_Push_Swap/ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: ottalhao <ottalhao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 13:57:17 by ottalhao          #+#    #+#             */
-/*   Updated: 2026/01/04 14:12:04 by ottalhao         ###   ########.fr       */
+/*   Updated: 2026/01/04 16:25:30 by ottalhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@ long long	ft_atol(const char *nptr)
 		i++;
 	}
 	while (nptr[i] && nptr[i] >= '0' && nptr[i] <= '9')
+	{
 		r = r * 10 + (nptr[i++] - '0');
+		if (r < -2147483648 || r > 2147483647)
+			break;
+	}
 	return (r * s);
 }
