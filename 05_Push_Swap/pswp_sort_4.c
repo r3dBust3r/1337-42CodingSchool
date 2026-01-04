1cfb80271aa9f3620e53ac8b59bffd9680f2bc1b
r3dBust3r <otmane.ki98@gmail.com>
2026-01-04 14:17:52 +0100

    norm: pswp_sort_4.c

--- 05_Push_Swap/pswp_sort_4.c
+++ 05_Push_Swap/pswp_sort_4.c
@@ -6,23 +6,27 @@
 /*   By: ottalhao <ottalhao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 11:38:53 by ottalhao          #+#    #+#             */
-/*   Updated: 2026/01/04 11:59:42 by ottalhao         ###   ########.fr       */
+/*   Updated: 2026/01/04 14:17:36 by ottalhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void pswp_sort_4(t_list **stack_a, t_list **stack_b)
+void	pswp_sort_4(t_list **stack_a, t_list **stack_b)
 {
-	t_list *smallest_node = *stack_a;
-	t_list *current = *stack_a;
+	t_list	*smallest_node;
+	t_list	*current;
+	int		distance;
+
+	smallest_node = *stack_a;
+	current = *stack_a;
 	while (current)
 	{
 		if (current->index < smallest_node->index)
 			smallest_node = current;
 		current = current->next;
 	}
-	int distance = find_distance(*stack_a, smallest_node->n);
+	distance = find_distance(*stack_a, smallest_node->n);
 	while (distance)
 	{
 		rotate_stack(stack_a, "ra");
