c62f46a366faca3ef6b3d979864559b6311b35c3
r3dBust3r <otmane.ki98@gmail.com>
2026-01-08 16:56:26 +0100

    Python_Module_00 - Fix: Ex07:elif

--- 06_Python_Module_00/ex7/ft_seed_inventory.py
+++ 06_Python_Module_00/ex7/ft_seed_inventory.py
@@ -6,16 +6,16 @@
 #    By: ottalhao <ottalhao@student.1337.ma>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/01/08 14:49:37 by ottalhao          #+#    #+#              #
-#    Updated: 2026/01/08 15:22:29 by ottalhao         ###   ########.fr        #
+#    Updated: 2026/01/08 16:52:50 by ottalhao         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     if (unit == "packets"):
         print(f"{seed_type.capitalize()} seeds: {quantity} packets available")
-    if (unit == "grams"):
+    elif (unit == "grams"):
         print(f"{seed_type.capitalize()} seeds: {quantity} grams total")
-    if (unit == "area"):
+    elif (unit == "area"):
         print(f"{seed_type.capitalize()} seeds: covers {quantity} square meters")
     else:
         print("Unknown unit type")
