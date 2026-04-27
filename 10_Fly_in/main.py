1ade5a0549fe6bf5183b026c33adfa78af5d5e9e
r3dBust3r <otmane.ki98@gmail.com>
2026-04-27 11:45:27 +0100

    PARSING_V2

--- 10_Fly_in/main.py
+++ 10_Fly_in/main.py
@@ -1,182 +1,220 @@
-"""
-    - [+] read file as lines
-    - [+] loop trought them
-    - [+] strip them
-    - [+] ignore comments #
-    - [+] split by (:)
-    - [+] if line starts with nb_drones, store them and continue
-    - [+] if line starts with start_hub, end_hub, hub, start extracting zones
-        - [+] split by space ( )
-            - [+] name      -> [0]
-            - [+] x         -> [1]
-            - [+] y         -> [2]
-            - [+] metadata  -> [3]
-                - [+] remove [  and  ]
-                - [+] split by: (=)
-                    if key in color, zone, max_drones
-                        - [+] {key[0]: value[1]}
+from pydantic import BaseModel, ValidationError, Field
+from typing import Literal, Dict, Optional
 
-    - [+] if line starts with connection, start extracting connections
-        - [+] splie by space: ( )
-        - [+] name              --> [0] 
-        - [+] max_link_capacity --> [1]
-            - [+] remove ([) and (])
-            - [+] split by: (=) 
-            - [+] grab [1]
-            - [+] convert into intg
-"""
 
+
+class ZoneMetadata(BaseModel):
+    color: Optional[str] = None
+    max_drones: Optional[int] = None
+
+
+class ZoneModel(BaseModel):
+    type: Literal['start_hub', 'hub', 'end_hub']
+    name: str = Field(min_length=1, max_length=250)
+    x: int
+    y: int
+    metadata: Optional[ZoneMetadata] = None
+
+
+class MaxLinkCapacity(BaseModel):
+    max_link_capacity: Optional[int] = None
+
+
+class ConnectionModel(BaseModel):
+    name: str = Field(min_length=1, max_length=250)
+    max_link_capacity: Optional[MaxLinkCapacity] = None
+    
 
 class Parser:
     def __init__(self, path):
-        self._map = []
-        self._nb_drones = 0
-        self._zones = []
-        self._connections = []
+        self.maps = []
+        self.nb_drones = 0
+        self.zones = []
+        self.connections = []
 
         try:
-            with open(path, 'rt') as file:
-                self._map = file.readlines()
-        except FileNotFoundError:
-            print(f'No fuch file: {path}')
-        except PermissionError:
-            print(f'No access permission for: {path}')
+            with open(path, 'r') as file:
+                self.maps = file.readlines()
         except Exception as e:
             print(e)
 
-        for line in self._map:
+
+    def extract_metadata(self, metadata):
+        metadata = metadata[1:-1].split(' ')
+
+        extracted = {}
+        for pair in metadata:
+            if '=' not in pair:
+                raise ValueError('Invalid metadata!')
+
+            key, value = pair.split('=')
+
+            if key not in ['max_link_capacity', 'max_drones', 'color']:
+                raise ValueError(f'Invalid key: {key}')
+
+            extracted[key] = int(value) if key in ['max_link_capacity', 'max_drones'] else value
+
+        return extracted
+
+
+    def print_zones(self):
+        for z in self.zones:
+            print(z)
+
+
+    def print_connections(self):
+        for c in self.connections:
+            print(c)
+
+
+    def parse(self):
+        for line in self.maps:
             line = line.strip()
             if line.startswith('#') or line == '':
                 continue
-            
-            line = line.split(':')
-            if len(line) != 2:
-                raise ValueError('Invalid map file!')
-            
-            key, value = line
 
-            key = key.strip().lower()
-            value = value.strip().lower()
 
-            name     = ''
-            hub_x    = 0
-            hub_y    = 0
-            metadata = {}
-
+            try:
+                key, value = line.split(':')
+                key = key.strip()
+                value = value.strip()
+            except ValueError:
+                raise ValueError('Invalid map structure!')
 
             if key == 'nb_drones':
+                if self.nb_drones:
+                    raise ValueError('Two entries for nb_drones')
+                
+                if self.zones or self.connections:
+                    raise ValueError('The first line must be: nb_drones')
+
                 try:
-                    self._nb_drones = int(value)
-                except Exception as e:
-                    print(e)
+                    self.nb_drones = int(value)
+                except ValueError:
+                    raise ValueError('nb_drones must be a number!')
 
-            elif key in ['start_hub', 'end_hub', 'hub']:
-                value = value.split(' ')
-                if len(value) < 3:
-                    raise ValueError('Invalid hub metadata!')
 
-                name     = value[0]
-                hub_x    = int(value[1])
-                hub_y    = int(value[2])
-                metadata = '[zone=normal]'
+            elif key in ['start_hub', 'hub', 'end_hub']:
 
-                if len(value) > 3:
-                    metadata = ' '.join(value[3:])
+                if '[' not in value:
+                    value += '[color=default]'
 
-                metadata = self.parse_metadata(metadata)
+                bracket_index = value.index('[')
 
-                if not metadata:
-                    raise ValueError('Invalid metadata structure!')
+                name_and_coords = value[:bracket_index].strip().split(' ')
+                metadata = value[bracket_index:].strip()
 
-                self._zones.append({
-                    'name': name,
-                    'hub_x': hub_x,
-                    'hub_y': hub_y,
-                    'metadata': metadata,
+                if len(name_and_coords) != 3:
+                    raise ValueError('Invalid zone structure!')
+
+                try:
+                    zone_name = name_and_coords[0]
+                    zone_x = int(name_and_coords[1])
+                    zone_y = int(name_and_coords[2])
+                except ValueError:
+                    raise ValueError('(X, Y) must be numbers!')
+
+
+                self.zones.append({
+                    'type': key,
+                    'name': zone_name,
+                    'x': zone_x,
+                    'y': zone_y,
+                    'metadata': self.extract_metadata(metadata),
                 })
+
 
             elif key == 'connection':
                 value = value.split(' ')
-                if len(value) not in [1, 2]:
-                    raise ValueError('Invalid connection!')
-                
-                name     = value[0]
-                metadata = '[max_link_capacity=1]'
+                if len(value) == 1:
+                    value.append('[max_link_capacity=1]')
 
-                metadata = self.parse_metadata(metadata)
+                try:
+                    name, max_link_capacity = value
+                except ValueError:
+                    raise ValueError('Invalid connection structure!')
 
-                if not metadata:
-                    raise ValueError('Invalid metadata structure!')
-
-                if len(value) == 2:
-                   metadata = value[1]
-
-                self._connections.append({
+                self.connections.append({
                     'name': name,
-                    'metadata': metadata,
+                    'metadata': self.extract_metadata(max_link_capacity),
                 })
-        ...
+
+            else:
+                raise ValueError(f'Invalid key detected: {key}')
 
 
-    def extract_map_data(self):
+        if not self.nb_drones:
+            raise ValueError('No nb_drones entry specified!')
+
+        if not self.zones:
+            raise ValueError('No zones entries specified!')
+
+        if not self.connections:
+            raise ValueError('No connections entries specified!')
+
+
+    def get_map(self):
         return {
-            'nb_drones': self._nb_drones,
-            'zones': self._zones,
-            'connections': self._connections
+            'nb_drones': self.nb_drones,
+            'zones': self.zones,
+            'connections': self.connections,
         }
-        ...
 
 
-    @staticmethod
-    def parse_metadata(metadata):
-        if not metadata.startswith('[') or not metadata.endswith(']'):
-            return None
+class Validate:
+    def __init__(self, _map):
+        self._map = _map
+        self.validated_zones = []
+        self.validated_connections = []
 
-        parsed_metadata = {}
-        keys = ['color', 'max_drones', 'max_link_capacity', 'zone']
-        zones = ['priority', 'normal', 'restricted', 'blocked']
+    def validate(self):
+        nb_drones   = self._map['nb_drones']
+        zones       = self._map['zones']
+        connections = self._map['connections']
 
-        try:
-            values = metadata[1:-1].strip().split(' ')
-            for val in values:
-                key, value = val.split('=')
-                if key not in keys:
-                    raise ValueError('Invalid key detected!')
-                
-                if key == 'zone' and value not in zones:
-                    raise ValueError('Invalid zone type detected!')
+        for zone in zones:
+            self.validated_zones.append(
+                ZoneModel(**zone)
+            )
 
-                parsed_metadata[key] = int(value) if key in ['max_drones', 'max_link_capacity'] else value
-        except Exception as e:
-            print(e)
-            return None
+        for connection in connections:
+            self.validated_connections.append(
+                ConnectionModel(**connection)
+            )
 
-        return parsed_metadata
-        ...
-    ...
+        if not isinstance(nb_drones, int):
+            raise ValueError('nb_drones must be an int!')
+
+        if nb_drones <= 0:
+            raise ValueError('nb_drones must be positive!')
+
 
 
 def main():
+    parser = Parser('maps/easy/01_linear_path.txt')
+    parser.parse()
+    _map = parser.get_map()
+
+
+    # try:
+    #     parser = Parser('maps/easy/01_linear_path.txt')
+    #     parser.parse()
+    # except ValueError as e:
+    #     print(e)
+
+
     try:
-        parser = Parser("maps/easy/01_linear_path.txt")
-    except Exception as e:
-        print(e)
-        exit(1)
+        validator = Validate(_map)
+        validator.validate()
+    except ValidationError as e:
+        print(f"Pydantic error: {e.errors()[0]['msg']}")
 
-    map = parser.extract_map_data()
 
-    for k, v in map.items():
-        print(f'[ {k} ]')
+    parser.print_zones()
+    print()
+    parser.print_connections()
 
-        if isinstance(v, list):
-            for item in v: print(item)
-        else: print(v)
-
-        print('-' * 42)
-        print()
-    ...
 
 
 if __name__ == "__main__":
     main()
-    ...
